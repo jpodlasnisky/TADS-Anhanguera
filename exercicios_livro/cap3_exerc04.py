c0931c0fb825f1d5278728e9147f90e653a20964
Podlasnisky <jpodlasnisky@gmail.com>
2020-09-16 20:22:02 -0300

    upload files

---
+++ exercicios_livro/cap3_exerc04.py
@@ -0,0 +1,7 @@
+numero = int(input('Digite um número inteiro posito ou negativo: '))
+if numero < 0:
+    print('Número negativo')
+elif numero == 0:
+    print('Zero é positivo???')
+else:
+    print('Número positivo')
---
+++ exercicios_livro/cap3_exerc06.py
@@ -0,0 +1,6 @@
+leia = []
+leia.append(int(input('digite o primeiro número ')))
+leia.append(int(input('digite o segundo número ')))
+leia.sort(reverse=True)
+print('o maior deles é: {}'.format(leia[0]))
+
